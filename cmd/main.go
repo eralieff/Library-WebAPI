2401533247a9cd8e0897838373ee980b0f93b62a
Berik Yeraliyev | 00058973 <BerikE@halykbank.kz>
2024-03-01 17:21:04 +0500

    Added "Hello world" simple app example with Fiber

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	fmt.Println("hello halyk")
+//}
+
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":8000")
+}
