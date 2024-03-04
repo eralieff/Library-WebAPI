0a66e9a15a28fac5b4de8c59b717c91c8037a308
Berik Yeraliyev | 00058973 <BerikE@halykbank.kz>
2024-03-04 17:09:35 +0500

    Added database configurations

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,7 +3,9 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	Port string `mapstructure:"PORT"`
+	Port     string `mapstructure:"PORT"`
+	Db       string `mapstructure:"DB"`
+	Password string `mapstructure:"DB_PASSWORD"`
 }
 
 func LoadConfig() (*Config, error) {
@@ -13,6 +15,15 @@ func LoadConfig() (*Config, error) {
 	v.AutomaticEnv()
 
 	err := v.BindEnv("PORT")
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.BindEnv("DB")
+	if err != nil {
+		return nil, err
+	}
+	err = v.BindEnv("DB_PASSWORD")
 	if err != nil {
 		return nil, err
 	}
