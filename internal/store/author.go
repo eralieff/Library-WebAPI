5468b92094d5ada2bebb36eb1491a6fbeeda96d1
Berik Yeraliyev | 00058973 <BerikE@halykbank.kz>
2024-03-14 09:32:19 +0500

    Updated the CreateAuthor function by adding changes to the validation, and completely changed and improved the UpdateAuthor function.

--- internal/store/author.go
+++ internal/store/author.go
@@ -2,10 +2,17 @@ package store
 
 import (
 	"Library_WebAPI/internal/model"
+	"errors"
 	"fmt"
+	"strings"
 )
 
 func (s *Store) CreateAuthor(author *model.Author) error {
+	if author.FullName == "" || author.Nickname == "" || author.Speciality == "" {
+		s.logger.Error("Error creating author: ", "one or more empty fields")
+		return errors.New("one or more empty fields")
+	}
+
 	_, err := s.db.Exec("INSERT INTO Author (full_name, nickname, speciality) VALUES ($1, $2, $3)", author.FullName, author.Nickname, author.Speciality)
 	if err != nil {
 		s.logger.Error("Error creating author", err)
@@ -41,6 +48,8 @@ func (s *Store) ReadAuthors() ([]model.Author, error) {
 	return authors, nil
 }
 
+// OLD: UpdateAuthor
+/*
 func (s *Store) UpdateAuthor(authorID int, updatedAuthor *model.Author) error {
 	result, err := s.db.Exec("UPDATE Author SET full_name = $1, nickname = $2, speciality = $3 WHERE id = $4", updatedAuthor.FullName, updatedAuthor.Nickname, updatedAuthor.Speciality, authorID)
 	if err != nil {
@@ -53,6 +62,59 @@ func (s *Store) UpdateAuthor(authorID int, updatedAuthor *model.Author) error {
 		s.logger.Error("Error getting rows affected: ", err)
 		return err
 	}
+	if rowsAffected == 0 {
+		s.logger.Error("Author with ID not found: ", authorID)
+		return fmt.Errorf("author with ID %d not found", authorID)
+	}
+
+	return nil
+}
+*/
+
+func (s *Store) UpdateAuthor(authorID int, updatedAuthor *model.Author) error {
+	query := "UPDATE Author SET"
+	var args []interface{}
+	var paramCounter = 1
+
+	if updatedAuthor.FullName != "" {
+		query += fmt.Sprintf(" full_name = $%d,", paramCounter)
+		args = append(args, updatedAuthor.FullName)
+		paramCounter++
+	}
+
+	if updatedAuthor.Nickname != "" {
+		query += fmt.Sprintf(" nickname = $%d,", paramCounter)
+		args = append(args, updatedAuthor.Nickname)
+		paramCounter++
+	}
+
+	if updatedAuthor.Speciality != "" {
+		query += fmt.Sprintf(" speciality = $%d,", paramCounter)
+		args = append(args, updatedAuthor.Speciality)
+		paramCounter++
+	}
+
+	if len(args) == 0 {
+		s.logger.Error("Error updating author: ", "empty request")
+		return errors.New("empty request")
+	}
+
+	queryString := strings.TrimSuffix(query, ",")
+	queryString += fmt.Sprintf(" WHERE id = $%d", paramCounter)
+	args = append(args, authorID)
+
+	result, err := s.db.Exec(queryString, args...)
+	if err != nil {
+		s.logger.Error("Error updating author: ", err)
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		s.logger.Error("Error getting rows affected: ", err)
+		return err
+	}
+
 	if rowsAffected == 0 {
 		s.logger.Error("Author with ID not found: ", authorID)
 		return fmt.Errorf("author with ID %d not found", authorID)
